3b66799e83ab2476415e1635a2cde9bab88c50b4
Javi Sanchez <144378200+javijsgithub@users.noreply.github.com>
2024-03-21 02:36:45 +0100

    crear header para redes, logo y botones de login, register y logout con link de usuario logueado

---
+++ musicolabo/src/components/header.js
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { MusiColaboContext } from '../context/context';
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
+
+import '../styles/header.css';
+
+const Header = () => {
+  const { handleLogout, loggedIn, userEmail } = useContext(MusiColaboContext);
+ 
+  return (
+    <div className='container-header'>
+      <div className='row-header'>
+        <div className='col col-redes'>
+          <FaInstagram /> <FaFacebook /> <FaSquareXTwitter /> <FaYoutube />
+        </div>
+        <div className=' col col-logo'>
+          <div className='musicolabo-logo'> 
+            <h1>MC</h1>
+          </div>
+            <h1>MusiColabo</h1>
+        </div>
+        <div className=' col col-buttons-header'>
+          {loggedIn ? (
+            <div className='container-user-logged-header'>
+              <Link to="/" onClick={handleLogout} className="btn btn-secondary" id='btn-header-logout'>Cerrar sesion</Link>
+              <p><Link to="/profile">{userEmail}</Link></p>
+            </div>
+          ) : (
+          <>
+              <Link to="/login" type="submit" className="btn btn-secondary" id='btn-header-login'>Iniciar sesión</Link>
+              <Link to="/register" type="submit" className="btn btn-secondary" id='btn-header-register'>Registrarse</Link>
+          </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
