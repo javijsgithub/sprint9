d45992c86cfc16fbb2b82a805e628549a6932921
Javi Sanchez <144378200+javijsgithub@users.noreply.github.com>
2024-03-22 02:14:20 +0100

    importar getFirestore y collection

--- musicolabo/src/firebase.js
+++ musicolabo/src/firebase.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getFirestore, collection } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,5 +23,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app, analytics);
+const db = getFirestore(app);
 
-export { auth };
+
+export { auth,db, collection };
