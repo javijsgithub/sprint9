6a37c58223bf419b84df3dfdf73705b3f15ab140
Javi Sanchez <144378200+javijsgithub@users.noreply.github.com>
2024-03-24 01:30:04 +0100

    agregamos getProfilesFromFirestore del context

--- musicolabo/src/components/usersList.js
+++ musicolabo/src/components/usersList.js
@@ -1,12 +1,31 @@
-import React/*, { useContext }*/ from 'react';
-//import { MusiColaboContext } from '../context/context';
+import React, { useContext, useEffect, useState } from 'react';
+import { MusiColaboContext } from '../context/context';
 import { Link } from 'react-router-dom';
 import Header from './header';
+import '../styles/userList.css';
 
 
 const UsersList = () => {
-    //const { users, loadMoreUsers } = useContext(MusiColaboContext);
+  const { getProfilesFromFirestore } = useContext(MusiColaboContext);
+  const [profiles, setProfiles] = useState([]);
+
+  useEffect(() => {
+    const fetchProfiles = async () => {
+      try {
+        const fetchedProfiles = await getProfilesFromFirestore();
+        console.log("Perfiles obtenidos:", fetchedProfiles);
+        setProfiles(fetchedProfiles);
+      } catch (error) {
+        console.error('Error al obtener perfiles:', error);
+      }
+    };
+
+    fetchProfiles();
+  }, [getProfilesFromFirestore]);
   
+  
+
+
     return (
      
       <div className='container-users-list'>
@@ -15,6 +34,17 @@ const UsersList = () => {
           <Link to="/" className="btn btn-secondary" id='btn-users-list-go-to-header'>Ir a la pagina de Bienvenida</Link>
 
           <h2>LISTADO</h2>
+
+          <ul>
+            {profiles.map(profile => (
+              <li key={profile.email}>
+                <h3>{profile.name}</h3>
+                <p><img src={profile.picture} alt="Imagen de perfil" /> </p>
+               <p>Instrumentos: {profile.instruments.join(', ')}</p>
+               {/* Mostrar otros detalles del perfil según sea necesario */}
+              </li>
+            ))}
+          </ul>
       </div>
             
           
