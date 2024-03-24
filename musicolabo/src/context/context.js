2f8e39543fac22d517f8a6a04a75a18b850f1f65
Javi Sanchez <144378200+javijsgithub@users.noreply.github.com>
2024-03-24 01:25:09 +0100

    funciones para ver a los usuarios en un listado y para subir imagenes a firebase firestorage

--- musicolabo/src/context/context.js
+++ musicolabo/src/context/context.js
@@ -1,14 +1,15 @@
 import React, { createContext, useState } from 'react';
-import { auth, db } from '../firebase';
-import { addDoc, collection } from 'firebase/firestore';
+import { auth, db, storage } from '../firebase';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
-
 export const MusiColaboContext = createContext();
 
 const MusiColaboContextProvider = ({ children }) => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [user, setUser] = useState(null);
   const [userEmail, setUserEmail] = useState('');
+
   
   // Función para registrar un nuevo usuario
   const handleRegister = async ({ email, password }) => {
@@ -45,13 +46,39 @@ const MusiColaboContextProvider = ({ children }) => {
     }
   };
 
-  // Función para crear un nuevo documento de perfil de usuario en Firestore Database
+  // Función para crear un nuevo documento de perfil de usuario en Firestore Database.
   const createNewDocument = async (userProfile) => {
     try {
-      await addDoc(collection(db, 'userData'), userProfile);
-      console.log('Documento creado exitosamente con los datos del perfil del usuario:', userProfile);
+      
+        await addDoc(collection(db, 'userData'), userProfile);
+        console.log('Documento creado exitosamente con los datos del perfil del usuario:', userProfile);
+      
     } catch (error) {
       console.error('Error al crear el documento:', error);
+      throw error;
+    }
+  };
+
+  const uploadImage = async (file) => {
+    try {
+      const storageRef = ref(storage, `images/${file.name}`);
+      const snapshot = await uploadBytes(storageRef, file);
+      console.log('Imagen subida correctamente:', file.name);
+      const downloadURL = await getDownloadURL(snapshot.ref);
+      return downloadURL; // Devuelve la URL de descarga de la imagen
+    } catch (error) {
+      console.error("Error al subir la imagen:", error);
+      throw error;
+    }
+  };
+
+  const getProfilesFromFirestore = async () => {
+    try {
+      const profilesSnapshot = await getDocs(collection(db, 'userData'));
+      const profiles = profilesSnapshot.docs.map(doc => doc.data());
+      return profiles;
+    } catch (error) {
+      console.error('Error al obtener perfiles de usuario:', error);
       throw error;
     }
   };
@@ -59,12 +86,15 @@ const MusiColaboContextProvider = ({ children }) => {
   return (
     <MusiColaboContext.Provider value={{ 
       user,
+      storage,
       userEmail,
       loggedIn,
+      uploadImage,
       createNewDocument,
       handleRegister,
       handleLogin,
-      handleLogout  
+      handleLogout, 
+      getProfilesFromFirestore 
       }}>
       {children}
     </MusiColaboContext.Provider>
