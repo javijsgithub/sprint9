8d1cb015e33f321c471a1b747e23a5a7c15224c4
Javi Sanchez <144378200+javijsgithub@users.noreply.github.com>
2024-03-28 14:57:31 +0100

    importar componente Messages y añadir ruta

--- musicolabo/src/App.js
+++ musicolabo/src/App.js
@@ -6,7 +6,8 @@ import Header from './components/header.js';
 import UsersList from './components/usersList.js';
 import WelcomeScreen from './components/welcomeScreen.js';
 import CreateProfile from './components/createProfile.js';
-import EditProfile from './components/editProfile';
+import EditProfile from './components/editProfile.js';
+import Messages from './components/messages.js';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
@@ -23,6 +24,7 @@ function App() {
             <Route path="/create-profile" element={<CreateProfile />} />
             <Route path="/edit-profile" element={<EditProfile />} />
             <Route path="/list" element={<UsersList/>} />
+            <Route path="/messages" element={<Messages />} />
             </Routes>
           </Router>
          </MusiColaboContextProvider>
