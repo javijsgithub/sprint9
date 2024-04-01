b3170cd209475db8e60a33ceafcf4aac122b8488
Javi Sanchez <144378200+javijsgithub@users.noreply.github.com>
2024-04-02 01:48:19 +0200

    crear rutas para ver perfil y ver videos de usuarios

--- musicolabo/src/App.js
+++ musicolabo/src/App.js
@@ -7,6 +7,8 @@ import UsersList from './components/usersList.js';
 import WelcomeScreen from './components/welcomeScreen.js';
 import CreateProfile from './components/createProfile.js';
 import EditProfile from './components/editProfile.js';
+import UserProfile from './components/userProfile.js';
+import UserVideos from './components/userVideos.js';
 import Messages from './components/messages.js';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
@@ -17,14 +19,16 @@ function App() {
       <MusiColaboContextProvider>
           <Router>
             <Routes>
-            <Route path="/" element={<WelcomeScreen />} />
-            <Route path="/header" element={<Header />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/create-profile" element={<CreateProfile />} />
-            <Route path="/profile" element={<EditProfile />} />
-            <Route path="/list" element={<UsersList/>} />
-            <Route path="/messages" element={<Messages />} />
+              <Route path="/" element={<WelcomeScreen />} />
+              <Route path="/header" element={<Header />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/create-profile" element={<CreateProfile />} />
+              <Route path="/edit-profile" element={<EditProfile />} />
+              <Route path="/list" element={<UsersList/>} />
+              <Route path="/messages" element={<Messages />} />
+              <Route path="/user-profile/:userEmail" element={<UserProfile />} />
+              <Route path="/user-videos/:userEmail" element={<UserVideos />} />
             </Routes>
           </Router>
          </MusiColaboContextProvider>
