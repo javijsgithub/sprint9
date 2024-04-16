3df491f1f3b196530ee67f3bbffc034dc48f2a44
Javi Sanchez <144378200+javijsgithub@users.noreply.github.com>
2024-04-17 00:05:21 +0200

    crear descripcion

--- musicolabo/src/components/welcomeScreen.js
+++ musicolabo/src/components/welcomeScreen.js
@@ -8,9 +8,16 @@ function WelcomeScreen() {
     return (
       <div className='container-welcomeScreen'>
          <Header />
-        <div className="container-welcome shadow p-3">
-        <Link to="/list" className="btn btn-secondary" id='btn-welcome-screen-go-to-user-list'>Ir al listado de MusiColabors</Link>
-          <h1>bienvenido</h1>
+        <div className="container-welcome">
+        <h2><b>Bienvenido a MusiColabo!</b></h2>
+        <hr></hr>
+          <p className="welcome-description">
+            MusiColabo es una plataforma diseñada para conectar músicos que buscan colaborar y crear juntos. Aquí, podrás explorar perfiles de diversos artistas, conocer sus habilidades, los instrumentos que dominan y qué están buscando o pueden ofrecer en una colaboración musical. 
+          </p>
+          <p className="welcome-description">
+            Navega libremente por el listado de MusiColabors para ver las tarjetas de perfil de cada usuario. Para optimizar tu búsqueda y ganar tiempo, puedes filtrar los perfiles por instrumento y ciudad, lo que te permitirá encontrar músicos que se ajusten específicamente a tus necesidades y ubicación. <br></br><br></br> Si decides registrarte, al iniciar sesion podrás acceder a funciones adicionales como ver videos de los usuarios y enviarles mensajes directos para iniciar una colaboración. <br></br>¡Únete a nuestra comunidad y comienza a crear música juntos!
+          </p>
+          <Link to="/list" className="btn btn-secondary" id='btn-welcome-screen-go-to-user-list'>Ir al listado de MusiColabors</Link>
         </div>
        </div>
     );
